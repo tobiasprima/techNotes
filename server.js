f02ed880c8f02895676c617171994753227d026d
Tobias Prima <primatjahja@gmail.com>
2024-02-10 22:18:33 +0700

    feat: route to 404 page

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root'));
 
+app.all('*', (req,res)=> {
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({message: '404 Not Found' });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+})
+
 app.listen(PORT, function(){
     console.log(`Server is listening on port ${PORT}`)
 })
