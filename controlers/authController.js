8d6568b0fe04ffbf31a22d7e5595654812051c51
Tobias Prima <primatjahja@gmail.com>
2024-02-23 00:16:01 +0700

    fix: fix typo

--- controlers/authController.js
+++ controlers/authController.js
@@ -13,7 +13,7 @@ const login = asyncHandler(async (req, res) => {
         return res.status(400).json({message: 'All field are required.'});
     }
 
-    const foundUser = await User.findOne({username}.exec());
+    const foundUser = await User.findOne({username}).exec();
 
     if(!foundUser || !foundUser.active){
         return res.status(401).json({message: 'Unauthorized.'})
