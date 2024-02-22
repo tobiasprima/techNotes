2a907f4eb7b394fa4d51f305a43a87f1a8a9bf4a
Tobias Prima <primatjahja@gmail.com>
2024-02-23 00:05:11 +0700

    feat: verify jwt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controlers/usersController");
+const verifyJWT = require("../middleware/verifyJWT");
+
+router.use(verifyJWT);
 
 router.route('/')
     .get(usersController.getAllUsers)
